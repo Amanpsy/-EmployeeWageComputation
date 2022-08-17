c22580f823e870fe7abef66b8488d3bf6ce52f1c
Aman <amankr97111@gmail.com>
2022-08-17 23:35:42 +0530

    fouth commit

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,6 +12,7 @@ namespace EmployeeWageComputation
         public const int emp_rate_per_hour = 20;
         public const int part = 1;
         public const int full = 2;
+        public const int total_working_days = 20;
 
         public static void Main(string[] args)
         {
@@ -19,32 +20,32 @@ namespace EmployeeWageComputation
             int empHour = 0;
 
             Random random = new Random();
-            int option = random.Next(0, 3);
-            switch(option)
+
+            for (int day = 1; day <= total_working_days; day++)
             {
-                case part:
-                    Console.WriteLine("Employee is working part time");
-                    empHour = 4;
-                    break;
+            int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case part:
+                        Console.WriteLine("Employee is working part time");
+                        empHour = 4;
+                        break;
 
-                case full:
-                    Console.WriteLine("employee is working full time");
-                    empHour = 8;
-                    break;
+                    case full:
+                        Console.WriteLine("employee is working full time");
+                        empHour = 8;
+                        break;
 
-                case 0:
-                    Console.WriteLine("employee is absent");
-                    break;
-           }
-            empWage = emp_rate_per_hour * empHour;
-            Console.WriteLine("the total employee wage is " + empWage);
+                    case 0:
+                        Console.WriteLine("employee is absent");
+                        break;
+                }
+                empWage = emp_rate_per_hour * empHour;
+                Console.WriteLine("the total employee wage is " + empWage);
+
+            }
 
         }
-
-
-
-
-
-    
     }
+
 }
